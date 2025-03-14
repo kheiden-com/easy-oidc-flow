39cca5cbfd44f88c3383f0a1baf155d251af3561
Kurt Heiden <kurt.w.heiden@gmail.com>
2025-03-13 19:16:10 -0600

    Add IDP Flask wrapper

--- example_server.py
+++ example_server.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 from easy_oidc_flow import EasyOIDCFlow
 
 
 app = EasyOIDCFlow(Flask(__name__))
-# app = Flask(__name__)
 
-def start():
-  app.run(host="0.0.0.0", port=8080, debug=True)
+@app.route("/")
+def main():
+  return f"hello {request.user_data["email"]}!"
 
 if __name__ in "__main__":
-  start()
+  app.run(host="0.0.0.0", port=8000, debug=True)
